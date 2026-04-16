42c3d4c216250e02809ad9b5b5e9c3d620c6e4b0
Shreyas Udaya <shreyasudaya03@gmail.com>
2026-04-15 20:59:14 -0400

    more structure

---
+++ dominators.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <cstdint>
+#include <numeric>
+#include <string>
+#include <vector>
+
+#include <llvm/ADT/BitVector.h>
+#include <llvm/ADT/DenseMap.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/CFG.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/PassPlugin.h>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace llvm;
+
+namespace {
+std::string getShortValueName(const Value* V) {
+  if (!V) return "(null)";
+  if (V->hasName()) return "%" + V->getName().str();
+  if (const auto* C = dyn_cast<ConstantInt>(V)) return std::to_string(C->getSExtValue());
+  std::string S;
+  raw_string_ostream OS(S);
+  V->printAsOperand(OS, false);
+  return S;
+}
+
+} //namespace
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION, "DominatorAnalysis", "v0.1",
+    [](PassBuilder& PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, FunctionPassManager& FPM, ArrayRef<PassBuilder::PipelineElement>) {
+          if (Name == "dominator-analysis") {
+            FPM.addPass(DominatorAnalysis());
+            return true;
+          }
+          return false;
+        }
+      );
+    }
+  };
+}
